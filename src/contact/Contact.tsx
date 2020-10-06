2133ad57177f35f9eb45caa0bd43117d41b06919
Vadim-w <vadim.morozov.2019@bk.ru>
2020-10-06 18:58:35 +0300

    Added form.

--- src/contact/Contact.tsx
+++ src/contact/Contact.tsx
@@ -1,20 +1,56 @@
-import React from 'react';
+import React, {useState} from 'react';
 import styles from './Contact.module.css'
-import styleContainer from "../common/styles/Container.module.css";
-import {Title} from "../common/components/title/Title";
+import styleContainer from '../common/styles/Container.module.css';
+import {Title} from '../common/components/title/Title';
+import axios from 'axios'
 
 export const Contact = () => {
+    let [contacts, setContacts] = useState<string>("")
+    let [name, setName] = useState<string>("")
+    let [message, setMessage] = useState<string>("")
+
+    const onChangeContacts = (value: string) => {
+        setContacts(value)
+    }
+    const onChangeName = (value: string) => {
+        setName(value)
+    }
+    const onChangeMessage = (value: string) => {
+        setMessage(value)
+    }
+
     return (
         <div className={styles.contactBlock}>
-            <div className= {`${styleContainer.container} ${styles.contactContainer}`}>
-                {/*<h2 className={styles.title}>Contact</h2>*/}
-                <Title title={"Contact"}/>
-                <form className={styles.form} action="">
-                    <input type="text"/>
-                    <input type="text"/>
-                    <textarea/>
+            <div className={`${styleContainer.container} ${styles.contactContainer}`}>
+                <Title title={'Contact'}/>
+                <form className={styles.form} action=''>
+                    <input
+                        type="text"
+                        placeholder={"Your email"}
+                        name='contacts'
+                        onChange={(e) => onChangeContacts(e.currentTarget.value)}
+                    />
+                    <input
+                        type="text"
+                        placeholder={'Your name'}
+                        name='name'
+                        onChange={(e) => onChangeName(e.currentTarget.value)}
+                    />
+                    <textarea
+                        placeholder={'Please write what you want'}
+                        name='message'
+                        onChange={(e) => onChangeMessage(e.currentTarget.value)}
+                    />
+                    <input type='submit' className={styles.button} onClick={(e) => {
+                        e.preventDefault();
+                        axios.post('https://smtp-server-node.herokuapp.com/sendMessage', {name, message, contacts})
+                            .then(() => {
+                                alert('Your message has been send :)')
+                            })
+                    }}/>
                 </form>
-                <button className={styles.button} type="submit">Send message</button>
+
+
             </div>
         </div>
     )
